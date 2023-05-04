76fc177c27297ada8e2139b514b11e022793fbd9
Punit Sharma <punit@tecorb.co>
2023-05-04 15:39:49 +0530

    remove db url

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const graphqlHTTP = require('express-graphql').graphqlHTTP;
 const schema = require('./schema/schema');
 const mongoose = require('mongoose'); 
 const cors = require('cors');
-mongoose.connect('mongodb+srv://imule:imule@cluster0-rekgb.mongodb.net/graphQl?retryWrites=true&w=majority'); 
+mongoose.connect('your db url'); 
 mongoose.connection.once('open', () => { 
    console.log('connected to database'); 
 }); 
@@ -15,15 +15,11 @@ app.use(cors());
 app.use('/graphql', graphqlHTTP({ 
    //directing express-graphql to use this schema to map out the graph 
    schema, 
-   //directing express-graphql to use graphiql when goto '/graphql' address in the browser
- 
-   //which provides an interface to make GraphQl queries
- 
+   //directing express-graphql to use graphiql when goto '/graphql' address in the browser 
+   //which provides an interface to make GraphQl queries 
    graphiql:true 
 })); 
  
-app.listen(3005, () => {
- 
-   console.log('Listening on port 3005');
- 
+app.listen(3005, () => { 
+   console.log('Listening on port 3005'); 
 });
